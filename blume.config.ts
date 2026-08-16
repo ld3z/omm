796d8ab8f092b9763f79e41759eb9e42d20eaf71
ld3z <ldeziangamer@gmail.com>
2026-08-16 08:35:21 -0700

    docs

--- blume.config.ts
+++ blume.config.ts
@@ -1,6 +1,46 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "blume";
 
 export default defineConfig({
   title: "Oh My Mathpad",
-  description: "Documentation powered by Blume.",
+  description: "Documentation for OMM",
+  logo: "/favicon.svg",
+  banner: {
+    content: "This documentation is subject to change.",
+    dismissible: true,
+    id: "v1"
+  },
+  integrations: [sitemap()],
+  lastModified: true,
+  dateFormat: { dateStyle: "medium" },
+
+  markdown: {
+    imageZoom: true,
+    code: {
+      icons: true,
+      wrap: false,
+    },
+    codeBlocks: {
+      theme: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
+
+  seo: {
+    og: { enabled: true },
+    rss: { enabled: true, types: ["blog", "changelog"] },
+    sitemap: true,
+    robots: true,
+    structuredData: true,
+  },
+
+  github: {
+    owner: "ld3z",
+    repo: "omm",
+  },
+  deployment: {
+    output: "static",
+  },
 });
