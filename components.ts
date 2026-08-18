7b0bb812caa8d46207c6c35c7def303099fc0fe0
ld3z <ldeziangamer@gmail.com>
2026-08-18 15:01:19 -0700

    download button

---
+++ components.ts
@@ -0,0 +1,8 @@
+import { defineComponents } from "blume";
+import DownloadButton from "./components/DownloadButton.astro";
+
+export default defineComponents({
+  mdx: {
+    DownloadButton,
+  },
+});
