8f782d6e674a6a9ec15be3d8ec1129e0461580d9
ld3z <ldeziangamer@gmail.com>
2026-09-24 14:31:08 -0700

    don't optimize my og imgs

--- jampack.config.mjs
+++ jampack.config.mjs
@@ -2,6 +2,8 @@ export default {
     image: {
         external: {
             process: 'download',
+            src_include: /docs\.scriptcat\.org/,
+            src_exclude: null,
         },
     },
 };
